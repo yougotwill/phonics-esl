663b2bc70aa6ddc5166c39b407f7a4ad2fb21bb0
William Grant <willmgrant@gmail.com>
2020-02-23 17:18:00 +0900

    replaced info icon with an svg

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 import humanReadableList from '../../utils/human-readable-list.js';
 
-import infoPng from '../../resources/icons/info.png';
-
 const Card = ({
   text,
   gestureImage,
@@ -32,7 +30,10 @@ const Card = ({
         <h3 className={secondary ? 'text-2xl font-bold select-none' : 'text-2xl select-none'}>{isPlaying === sound ? <span className='text-4xl'>◼</span> : text}</h3>
         <div className='absolute text-center tooltip-text' onClick={(event) => { onCardClick(event); }}>
           <div className='py-2 px-auto rounded-full'>
-            <p>{humanReadableList(examples)}<img className='w-6 h-6 mb-1 ml-2 inline' src={infoPng} alt='info' /></p>
+            <p>
+              {humanReadableList(examples)}
+              <svg className='mb-1 ml-4 inline' xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 24 24' fill='currentColor'><path d='M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-.001 5.75c.69 0 1.251.56 1.251 1.25s-.561 1.25-1.251 1.25-1.249-.56-1.249-1.25.559-1.25 1.249-1.25zm2.001 12.25h-4v-1c.484-.179 1-.201 1-.735v-4.467c0-.534-.516-.618-1-.797v-1h3v6.265c0 .535.517.558 1 .735v.999z' /></svg>
+            </p>
           </div>
           <p className='mx-auto tooltip-triangle' />
         </div>
