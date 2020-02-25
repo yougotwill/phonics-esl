76b5907a938f2f3946e8ea60535c48a65713f547
William Grant <yougotwill94@gmail.com>
2020-02-25 11:24:43 +0900

    investigated copy right, updated footer, mades notes about project FUTURE in TODO.md

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className='container flex flex-col md:flex-row justify-between items-center mx-auto pt-8'>
-      <span className='mb-5'>Built with <span role='img' aria-label='coffee'>☕️</span> and <span role='img' aria-label='heart'>❤️ by <a className='hover:underline' href='https://yougotwill.github.io/' target='_blank' rel='noopener noreferrer'>William Grant</a></span></span>
-      <a className='mb-8' href='mailto:yougotwill94@gmail.com' target='_blank' rel='noopener noreferrer'>Found a bug?</a>
+    <footer className='container mx-auto pt-8'>
+      <div className='flex flex-col justify-center items-center text-center'>
+       <p className='my-2 text-base'>All sounds, gesture images and example sentences come from the <a className='text-green-600' href='https://www.jollylearning.co.uk/resource-bank/' target='_blank' without rel="noopener noreferrer">Jolly Phonics Resource Bank</a> which is Copyright © 2018 Jolly Learning Ltd. All Rights Reserved.</p>
+       <p className='my-2 text-base'>This website is built with <a className='text-green-600' href='https://reactjs.org/' target='_blank' without rel="noopener noreferrer">React</a> and <a className='text-green-600' href='https://tailwindcss.com/' target='_blank' without rel="noopener noreferrer">TailwindCSS</a> and is open-source.<br/>See the source code <a className='text-green-600' href='https://github.com/yougotwill/phonics-esl' target='_blank' without rel="noopener noreferrer">here</a>.</p>
+      </div>
+      <div className='flex flex-col md:flex-row justify-between items-center mt-8'>
+        <span className='mb-5'>Built with <span role='img' aria-label='coffee'>☕️</span> and <span role='img' aria-label='heart'>❤️ by <a className='hover:underline' href='https://yougotwill.github.io/' target='_blank' rel='noopener noreferrer'>William Grant</a></span></span>
+        <a className='mb-8 hover:underline' href='mailto:yougotwill94@gmail.com' target='_blank' rel='noopener noreferrer'>Found a bug?</a>
+      </div>
     </footer>
   );
 };
