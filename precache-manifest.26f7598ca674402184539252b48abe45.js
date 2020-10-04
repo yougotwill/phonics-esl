ed64405ff148815e05f2560b90ddca0449b5e6b3
William Grant <willmgrant@gmail.com>
2020-10-04 11:05:04 +0900

    Updates

---
+++ precache-manifest.26f7598ca674402184539252b48abe45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc7310fc25d9e6e004e20fe0eb853bcb",
+    "url": "/phonics-esl/index.html"
+  },
+  {
+    "revision": "ec59c223d107f1366c50",
+    "url": "/phonics-esl/static/css/main.b52e5708.chunk.css"
+  },
+  {
+    "revision": "b855188a57963463dfb5",
+    "url": "/phonics-esl/static/js/2.402b215a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/phonics-esl/static/js/2.402b215a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec59c223d107f1366c50",
+    "url": "/phonics-esl/static/js/main.58999124.chunk.js"
+  },
+  {
+    "revision": "2f3f813c616288d9dedb",
+    "url": "/phonics-esl/static/js/runtime-main.d5408594.js"
+  }
+]);
