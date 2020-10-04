5bf0851b64fda59f34a23d190a3df1ea871b0613
William Grant <willmgrant@gmail.com>
2020-10-04 10:05:20 +0900

    added github templates, updated index.html and manifest

---
+++ .github/ISSUE_TEMPLATE/bug_report.md
@@ -0,0 +1,17 @@
+---
+name: Bug report
+about: Create a report to help us improve
+labels: bug
+---
+<!-- ⚠️⚠️ Do Not Delete This! bug_report_template ⚠️⚠️ -->
+<!-- Please search existing issues to avoid creating duplicates. -->
+
+<!-- Use Help > Report Issue to prefill these. -->
+- Browser Name:
+- Browser Version:
+- OS Version:
+
+Steps to Reproduce:
+
+1.
+2.
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -10,7 +10,7 @@ const Footer = () => {
       </div>
       <div className='flex flex-col md:flex-row justify-between items-center mt-8'>
         <span className='mb-5'>Built with <span role='img' aria-label='coffee'>☕️</span> and <span role='img' aria-label='heart'>❤️ by <a className='hover:underline' href='https://yougotwill.github.io/' target='_blank' rel='noopener noreferrer'>William Grant</a></span></span>
-        <a className='mb-8 hover:underline' href='mailto:yougotwill94@gmail.com' target='_blank' rel='noopener noreferrer'>Found a bug?</a>
+        <a className='mb-8 hover:underline' href='https://github.com/yougotwill/phonics-esl/issues/new?labels=bug&template=bug_report.md' target='_blank' rel='noopener noreferrer'>Found a bug?</a>
       </div>
     </footer>
   );
